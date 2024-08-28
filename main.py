6e7baff68ec806c486ed49ae6d909d922a538a29
Law <stolerulaurentiu2@gmail.com>
2024-08-28 16:46:57 +0300

    nothing important

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from tkinter import *
 import requests
 from api_key import API_KEY
-from tkinter import messagebox  # Import messagebox
+from tkinter import messagebox  
 
 def Generate():
-    category = 'happiness'  # Set a default category or use input
+    category = '' 
     api_url = f'https://api.api-ninjas.com/v1/quotes?category={category}'
     response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     
     if response.ok:
         quotes_data = response.json()
-        if quotes_data:  # Check if there are quotes
-            # Display the first quote from the list
+        if quotes_data:  
+           
             quote_info = quotes_data[0]
             quote = quote_info.get('quote', 'No quote available')
+            # category = quote_info.get('category', 'No quote available')
             author = quote_info.get('author', 'Unknown author')
             messagebox.showinfo("Quote", f"Quote: {quote}\nAuthor: {author}")
+      
         else:
             messagebox.showinfo("Quote", "No quotes found.")
     else:
         messagebox.showerror("Error", f"Error: {response.status_code} - {response.text}")
 
-# Initialize Tkinter window
+
 win = Tk()
 win.title("Quote Generator")
 
@@ -35,7 +37,7 @@ win.grid_rowconfigure(0, weight=1)
 win.grid_rowconfigure(1, weight=1)
 win.grid_rowconfigure(2, weight=1)
 
-# Create and place the button
+
 bntGen = Button(win, text="Generate", command=Generate)
 bntGen.grid(column=1, row=1, sticky="nsew")
 
